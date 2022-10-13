bc968c548a45b671f3a565affd21e733f1f8c6ef
Ryuk-the-Shinigami <sarumiadedamola@gmail.com>
2022-10-13 16:57:53 -0700

    CP5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  *main- Serves as the entry point for the program
+ *  *main- serves as the entry point
  *   *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
- *     */
+ *    *Return: zero means successful and not zero means error
+ *    */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}}
