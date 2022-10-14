cc67fe0abc15358013efb3bd09eb7cc3fde53a63
Ryuk-the-Shinigami <sarumiadedamola@gmail.com>
2022-10-13 17:15:32 -0700

    CP4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  *main- serves as the entry point
- *   *
- *    *Return: zero means successful and not zero means error
- *    */
-int main(void)
-{
+ *main- serves as the entry point
+ *
+ *Return: zero means successful and not zero means error
+ */
+ int main(void)
+ {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
-}}
+ }
